2e8e1c62a28d2cd7b4ca22f52b9db0d57c50023f
叁贰壹 <1943191086@qq.com>
2021-08-19 14:37:49 +0800

    cli-color

--- server/index.js
+++ server/index.js
@@ -1,5 +1,6 @@
 const fs = require('fs');
 const path = require('path');
+const clc = require("cli-color");
 
 // 创建 express 服务
 const express = require('express');
@@ -19,26 +20,26 @@ const renderer = createBundleRenderer(serverBundle, {
 
 // 处理静态文件请求
 server.use(express.static(path.resolve(__dirname, '../dist/client'), {
-    index: false
+    index: false // 不设置为 false 会导致无法查看网页源代码，搜索引擎将无法爬取
 }));
 
-
+// 通用服务 ==> 查看网页都是 GET 请求
 server.get('*', async (req, res) => {
     try {
         const context = {
-            url: req.url,
-            title: ``,
-            metas: ``,
+            url: req.url, // 请求 url ==> router.push(url)
+            title: ``, // 网页标题
+            metas: ``, // 写网站关键词，网站描述，给爬虫看
         }
         const html = await renderer.renderToString(context);
         res.send(html);
     } catch (error) {
-        console.log(error);
+        console.log(clc.red.bold(error));
         res.status(500).end('Internal Server Error')
     }
 });
 
-
+// 监听服务
 server.listen(3000, _ => {
-    console.log("http://localhost:3000");
+    console.log(clc.green.underline.bold("http://localhost:3000"));
 });
