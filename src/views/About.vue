e6c54f71ab4c5a5821a4ce0f1d7f256f98fd99e2
宋国豪 <songgh@dist.com.cn>
2021-09-09 17:50:18 +0800

    hot

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,20 @@
 <template>
   <div class="about">
     <h1>This is an about page</h1>
+    <button @click="saveData">保存</button>
+    <button @click="getData">弹框</button>
   </div>
 </template>
+<script>
+export default {
+  methods: {
+    saveData() {
+      window.localStorage.setItem("abc", JSON.stringify({ aaa: "111" }));
+    },
+    getData() {
+      let data = window.localStorage.getItem("abc");
+      alert(data);
+    },
+  },
+};
+</script>
